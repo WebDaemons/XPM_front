2892761e0569ba3574b16fd2e7974a6c6c4e2c7c
YanKeeV <nazar2k10@gmail.com>
2024-07-28 23:29:27 +0300

    greeting page initial commit

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import styles from './home.module.css';
+import { CgArrowLongRight } from "react-icons/cg";
+import { NavLink } from 'react-router-dom';
 
 function Home() {
-  return <div>Home</div>;
+
+  return(
+    <div className={styles.homeWrapper}>
+      <div className={styles.homeHeader}>
+        <h1 className={styles.homeName}><span style={{color:'#1B76FF'}}>XP</span>Manager</h1>
+        <NavLink to='/login' key='Login'><button className={styles.homeButton}>Sign In</button></NavLink>
+      </div>
+      <div className={styles.homeContent}>
+        <p className={styles.homeGreatings}>Greetings</p>
+        <div className={styles.homeRegisterHint}>
+          <div className={styles.homeHint}>if you're new here, start here</div>
+          <CgArrowLongRight size={50} />
+          <NavLink to='/signup' key='Registration'><button className={styles.homeButton}>Register</button></NavLink>
+        </div>
+        <div className={styles.homeInfoContainer}>
+          <div className={styles.homeInfo}>
+            <p style={{fontWeight:'700'}}>What we did</p>
+            <p>Basic user settings</p>
+            <p>To-do list</p>
+          </div>
+          <div className={styles.homeInfo}>
+            <p style={{fontWeight:'700'}}>What we're going to do</p>
+            <p>Theme swich</p>
+            <p>Notes</p>
+            <p>Share your to-do`s and notes</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
