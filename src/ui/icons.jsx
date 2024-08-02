252ce78cc6923761a7003354740fce63ff2b470a
Michael Verkhovod <michaelverkhovod@gmail.com>
2024-08-03 01:56:15 +0300

    export burrel for icons

---
+++ src/ui/icons.jsx
@@ -0,0 +1,4 @@
+export { PiDotsSixVertical, PiCalendarDotsLight } from 'react-icons/pi';
+export { MdKeyboardArrowDown, MdOutlineDelete } from 'react-icons/md';
+export { RxDotsVertical } from 'react-icons/rx';
+export { IoIosClose, IoIosAddCircleOutline } from 'react-icons/io';
