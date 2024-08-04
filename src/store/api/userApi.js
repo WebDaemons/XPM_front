cc5a60607a19a96de07e877d1b522afa403ef6e1
YanKeeV <nazar2k10@gmail.com>
2024-08-04 16:28:54 +0300

    user redux initial + sidebar logic

---
+++ src/store/api/userApi.js
@@ -0,0 +1,21 @@
+import axiosInstance from './axiosInstance';
+
+export const getUser = async (token) => {
+    const response = await axiosInstance.get('/core/user', {
+        headers: {
+          Authorization: `Bearer ${token}` 
+        }
+      });
+    console.log(response)
+    return response;
+  };
+  
+
+export const updateUser = async (data, token) => {
+    const response = await axios.post('//localhost:5000/skills/post', data, {
+      headers: {
+        Authorization: `Bearer ${token}` 
+      }
+    });
+    return response.data;
+};
