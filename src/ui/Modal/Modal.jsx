b0ce487c17153b6d0b6a2a15548df5a05b637868
Michael Verkhovod <michaelverkhovod@gmail.com>
2024-08-04 16:34:36 +0300

    expriment

--- src/ui/Modal/Modal.jsx
+++ src/ui/Modal/Modal.jsx
@@ -7,6 +7,8 @@ export const Modal = ({ isOpen, onClose, onSave, modalType, categories }) => {
   const [category, setCategory] = useState('');
   const [date, setDate] = useState('');
 
+  // eahahahaheah
+
   const taskModal = [
     {
       header: 'Add task',
