2b25eeefcaf77defa7b4078df57ffbca72c4051d
YanKeeV <nazar2k10@gmail.com>
2024-08-04 16:29:39 +0300

    sign in form + auth context with redux

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useState } from 'react';
 import styles from './signInForm.module.css';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { Button, Input, ErrorMessage } from '@ui/index';
 import { MdOutlineMail } from 'react-icons/md';
 import { FiLock } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 
 
 function SignInForm() {
@@ -22,14 +24,39 @@ function SignInForm() {
     },
   });
 
+  const { login } = useAuth();
+
+  const dispatch = useDispatch();
+
   const [checked, setChecked] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const error = useSelector((state) => state.auth.error);
+
+  const handleEmailChange = (value) => {
+    console.log('Input value:', value);
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (value) => {
+    console.log('Input value:', value);
+    setPassword(value);
+  };
 
   const handleCheckboxChange = () => {
     setChecked(!checked);
   };
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = () => {
+    const credentials = {
+      user: {
+        email,
+        password,
+      },
+    };
+    login(credentials);
   };
 
   return (
@@ -39,7 +66,9 @@ function SignInForm() {
     >
       <div className={styles.formHeader}>
         <h1 className={styles.headerName}>Sign In</h1>
-        <h2 className={styles.headerText}>Enter your email address and password to get access to account</h2>
+        <h2 className={styles.headerText}>
+          Enter your email address and password to get access to account
+        </h2>
       </div>
       <Input
         name="email"
@@ -48,6 +77,7 @@ function SignInForm() {
         placeholder="Email"
         icon={MdOutlineMail}
         showPasswordToggle
+        onValueChange={handleEmailChange}
       />
       {/* <ErrorMessage label="Field can`t be empty." /> */}
       <Input
@@ -57,6 +87,7 @@ function SignInForm() {
         placeholder="Password"
         icon={FiLock}
         showPasswordToggle
+        onValueChange={handlePasswordChange}
       />
       <div className={styles.signInOptions}>
         <label>
@@ -67,13 +98,18 @@ function SignInForm() {
           />
           Remember me
         </label>
-        <NavLink to='/login/resetpassword'><span style={{color:"#1B76FF"}}>Forgot password?</span></NavLink>
+        <NavLink to='/login/resetpassword'>
+          <span style={{color:"#1B76FF"}}>
+            Forgot password?
+          </span>
+        </NavLink>
       </div>
       <Button
         label="Sign In"
         type="submit"
         variant="mainButton"
       />
+      <h3 className={styles.loginError}>{error}</h3>
       <div className={styles.orDelimiter}>
         <span>OR</span>
       </div>
