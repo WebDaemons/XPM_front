9f782591687bc857fe018681b75f23f3d49d8c7d
Michael Verkhovod <michaelverkhovod@gmail.com>
2024-09-05 02:34:14 +0300

    import/export format

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,28 @@
+export * from './Sidebar/Sidebar';
+export * from './TabsBar/TabsBar';
+
+// AUTH PAGE
+
 export * from './AuthForm/AuthForm';
+export * from './SignInForm/SignInForm';
+
+// TODOLIST PAGE
+
 export * from './Category/Category';
 export * from './CategoryListItem/CategoryListItem';
-export * from './Sidebar/Sidebar';
-export * from './SignInForm/SignInForm';
 export * from './TaskListItem/TaskListItem';
+
+// NOTES PAGE
+
+export * from './AddNoteButton/AddNoteButton';
+export * from './AddNoteModal/AddNoteModal';
+export * from './EditNoteModal/EditNoteModal';
+export * from './NoteItem/NoteItem';
+export * from './NotesList/NotesList';
+
+// SETTINGS PAGE
+
+export * from './SettingsAppearance/SettingsAppearance';
+export * from './SettingsPassword/SettingsPassword.jsx';
+export * from './SettingsProfile/SettingsProfile';
+export * from './ThemeCard/ThemeCard';
