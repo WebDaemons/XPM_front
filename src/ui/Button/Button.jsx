51e5d3d26a0d811cd012cb30a0198ef861a10e15
Michael Verkhovod <michaelverkhovod@gmail.com>
2024-09-06 23:19:37 +0300

    button 90% ready

--- src/ui/Button/Button.jsx
+++ src/ui/Button/Button.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import styles from './button.module.css';
 
-export const Button = ({ label, type = 'button', icon, variant }) => {
-  const className =
-    variant === 'signByButton' ? styles.signByButton : styles.mainButton;
+export const Button = ({
+  children,
+  type = 'button',
+  disabled = false,
+  variant = 'contained',
+  size = 'md',
+  startIcon,
+  endIcon,
+  onClick,
+}) => {
+  const StartIcon = startIcon;
+  const EndIcon = endIcon;
+
+  // size: sm || md || lg
+  // variant: text || contained || outline
 
   return (
     <button
-      className={`${styles.button} ${className}`}
+      className={`${styles.button} ${styles[size]} ${styles[variant]}`}
       type={type}
+      disabled={disabled}
+      onClick={onClick}
     >
-      {icon && (
-        <img
-          src={icon}
-          className={styles.buttonIcon}
-        />
-      )}
-      {label}
+      {startIcon && <StartIcon />}
+      {children}
+      {endIcon && <EndIcon />}
     </button>
   );
 };
