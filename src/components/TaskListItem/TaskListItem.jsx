5bb3d1fa1e016b28395538bec480fb588cfcbc3f
Michael Verkhovod <michaelverkhovod@gmail.com>
2024-10-04 21:43:31 +0300

    task list upd

--- src/components/TaskListItem/TaskListItem.jsx
+++ src/components/TaskListItem/TaskListItem.jsx
@@ -8,23 +8,38 @@ import {
 } from '@ui/icons';
 import styles from './taskListItem.module.css';
 import { DropDown, Checkbox } from '@ui/index';
+import { DatePick } from '@components/index';
 
 export const TaskListItem = ({
   task,
   taskId,
   handleDeleteTask,
   handleOptionSelect,
+  handleToggleTaskStatus,
   options,
 }) => (
-  <div className={styles.taskListItem}>
+  <div
+    className={styles.taskListItem}
+    style={{
+      color: task.is_done ? 'gray' : 'black',
+    }}
+  >
     <div>
       <PiDotsSixVertical
         size={16}
         className={styles.dragIcon}
       />
     </div>
-    <Checkbox />
-    <div className={styles.name}>{task.name}</div>
+    <Checkbox
+      isChecked={task.is_done}
+      onChange={() => handleToggleTaskStatus(task.id)}
+    />
+    <div
+      className={styles.name}
+      style={{ textDecoration: task.is_done ? 'line-through' : 'none' }}
+    >
+      {task.name}
+    </div>
     <div className={styles.priority}>
       <DropDown options={options} />
       <IoIosClose
@@ -36,7 +51,7 @@ export const TaskListItem = ({
         color="#787c99"
       /> */}
     </div>
-    <div className={styles.dueDate}></div>
+    <div className={styles.dueDate}>{/* <DatePick /> */}</div>
     <div className={styles.createdAt}>{task.created_at.split('T')[0]}</div>
     <div>
       <MdOutlineDelete
