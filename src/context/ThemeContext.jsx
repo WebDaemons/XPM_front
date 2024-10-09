2dcf490eb84fa688a196fcf86c1ac16121bfa614
YanKeeV <nazar2k10@gmail.com>
2024-10-09 11:31:49 +0300

    system theme

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,7 +3,6 @@ import React, { createContext, useState, useEffect, useContext } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-
   const getInitialTheme = () => {
     const savedTheme = localStorage.getItem("theme");
     return savedTheme ? savedTheme : "light";
@@ -11,10 +10,36 @@ export const ThemeProvider = ({ children }) => {
 
   const [theme, setTheme] = useState(getInitialTheme);
 
+  const getSystemTheme = () => {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return prefersDark ? "dark" : "light";
+  };
+
   const setNewTheme = (newTheme) => {
     setTheme(newTheme);
     localStorage.setItem("theme", newTheme);
   };
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    
+    const handleChange = (e) => {
+      if (theme === "system") {
+        setTheme(getSystemTheme());
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    console.log(mediaQuery)
+
+    if (theme === "system") {
+      setTheme(getSystemTheme());
+    }
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, [theme]);
 
   useEffect(() => {
     localStorage.setItem("theme", theme);
