bd418e0b616967e498ee9616ae61bcdbb7a7fb06
Mykhailo Verkhovod <michaelverkhovod@gmail.com>
2026-06-01 21:51:30 +0300

    updated addtag and searchtag design + basic state

--- src/features/notes/components/TagManager v2/TagManager.jsx
+++ src/features/notes/components/TagManager v2/TagManager.jsx
@@ -5,9 +5,13 @@ import { HiHashtag } from 'react-icons/hi';
 import { IoIosArrowDown } from 'react-icons/io';
 import { hexToRgba } from '@utils/hexToRgba';
 import { Plus } from 'lucide-react';
+import { Button } from '@ui/index';
+import { ColorPicker } from '@components/index';
 
 export const TagManager = ({ tags }) => {
   const [tagManagerState, setTagManagerState] = useState('searchTag');
+  const [isSearchTagOpened, setIsSearchTagOpened] = useState(false);
+  const [color, setColor] = useState('#4F7CFF');
 
   return (
     <div className={styles.tagManagerWrapper}>
@@ -32,67 +36,106 @@ export const TagManager = ({ tags }) => {
               </span>
             </li>
           ))}
-          <button className={styles.openSearchbarBtn}>
+          <button
+            className={styles.openSearchbarBtn}
+            onClick={() => setIsSearchTagOpened((prev) => !prev)}
+          >
             <span>
-              <Plus />
+              <Plus
+                style={{
+                  transform: isSearchTagOpened ? 'rotate(45deg)' : '',
+                }}
+              />
             </span>
           </button>
         </ul>
       </div>
-      <div className={styles.searchTagWrapper}>
-        <span>
-          <HiHashtag />
-        </span>
-        <input
-          type="text"
-          placeholder="Add tags..."
-        />
-      </div>
-      <div className={styles.expandedTagsMenu}>
-        <span className={styles.tagType}>Recent</span>
-        <ul className={styles.availableTagsList}>
-          <li className={styles.availableTag}>
-            <span
-              style={{
-                color: '#800080',
-                // backgroundColor: hexToRgba('#800080', 0.075),
-                backgroundColor: '#f7eef6',
-              }}
-            >
-              <HiHashtag />
-            </span>
-            Job
-          </li>
-          <li className={styles.availableTag}>
-            <span
-              style={{
-                color: '#fa8008',
-                backgroundColor: hexToRgba('#fa8008', 0.075),
-              }}
-            >
-              <HiHashtag />
-            </span>
-            Sport
-          </li>
-          <li className={styles.availableTag}>
-            <span
-              style={{
-                color: '#1f790f',
-                backgroundColor: hexToRgba('#1f790f', 0.075),
-              }}
-            >
-              <HiHashtag />
-            </span>
-            Programmin
-          </li>
-        </ul>
-        <div className={styles.addNewTag}>
-          <span>
-            <Plus />
+      {isSearchTagOpened && (
+        <div className={styles.searchTagWrapper}>
+          <span
+            style={{
+              color:
+                tagManagerState == 'createTag' ? color : 'var(--text-color)',
+            }}
+          >
+            <HiHashtag />
           </span>
-          Add new tag
+          <input
+            type="text"
+            placeholder={
+              tagManagerState == 'searchTag'
+                ? 'Search tags...'
+                : 'Create tag...'
+            }
+          />
         </div>
-      </div>
+      )}
+      {tagManagerState == 'searchTag' ? (
+        <div className={styles.expandedTagsMenu}>
+          <span className={styles.tagType}>Recent</span>
+          <ul className={styles.availableTagsList}>
+            <li className={styles.availableTag}>
+              <span
+                style={{
+                  color: '#800080',
+                  // backgroundColor: hexToRgba('#800080', 0.075),
+                  backgroundColor: '#f7eef6',
+                }}
+              >
+                <HiHashtag />
+              </span>
+              Job
+            </li>
+            <li className={styles.availableTag}>
+              <span
+                style={{
+                  color: '#fa8008',
+                  backgroundColor: hexToRgba('#fa8008', 0.075),
+                }}
+              >
+                <HiHashtag />
+              </span>
+              Sport
+            </li>
+            <li className={styles.availableTag}>
+              <span
+                style={{
+                  color: '#1f790f',
+                  backgroundColor: hexToRgba('#1f790f', 0.075),
+                }}
+              >
+                <HiHashtag />
+              </span>
+              Programmin
+            </li>
+          </ul>
+          <button
+            className={styles.addNewTag}
+            onClick={() => setTagManagerState('createTag')}
+          >
+            <span>
+              <Plus />
+            </span>
+            Add new tag
+          </button>
+        </div>
+      ) : (
+        <div className={styles.creationTagWrapper}>
+          <ColorPicker
+            value={color}
+            onChange={setColor}
+          />
+          <div className={styles.creationTagActionBtns}>
+            <Button
+              onClick={() => setTagManagerState('searchTag')}
+              variant="outlined"
+            >
+              Cancel
+            </Button>
+            <Button>Create</Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
