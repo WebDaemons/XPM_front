15c781f364e1cf27d223c2729e2a05c90cd55c7c
Mykhailo Verkhovod <michaelverkhovod@gmail.com>
2026-06-02 17:49:36 +0300

    tag manager fixed showing options based on mode state

--- src/features/notes/components/TagManager v2/TagManager.jsx
+++ src/features/notes/components/TagManager v2/TagManager.jsx
@@ -9,7 +9,7 @@ import { Button } from '@ui/index';
 import { ColorPicker } from '@components/index';
 
 export const TagManager = ({ tags }) => {
-  const [tagManagerState, setTagManagerState] = useState('searchTag');
+  const [mode, setMode] = useState('none');
   const [isSearchTagOpened, setIsSearchTagOpened] = useState(false);
   const [color, setColor] = useState('#4F7CFF');
 
@@ -38,12 +38,20 @@ export const TagManager = ({ tags }) => {
           ))}
           <button
             className={styles.openSearchbarBtn}
-            onClick={() => setIsSearchTagOpened((prev) => !prev)}
+            onClick={() => {
+              setIsSearchTagOpened((prev) => !prev);
+
+              if (!isSearchTagOpened) {
+                setMode('none');
+              }
+            }}
           >
             <span>
               <Plus
                 style={{
-                  transform: isSearchTagOpened ? 'rotate(45deg)' : '',
+                  transform: isSearchTagOpened
+                    ? 'rotate(45deg)'
+                    : 'rotate(0deg)',
                 }}
               />
             </span>
@@ -54,87 +62,89 @@ export const TagManager = ({ tags }) => {
         <div className={styles.searchTagWrapper}>
           <span
             style={{
-              color:
-                tagManagerState == 'createTag' ? color : 'var(--text-color)',
+              color: mode == 'create' ? color : 'var(--text-color)',
             }}
           >
             <HiHashtag />
           </span>
           <input
             type="text"
-            placeholder={
-              tagManagerState == 'searchTag'
-                ? 'Search tags...'
-                : 'Create tag...'
-            }
+            placeholder={mode == 'search' ? 'Search tags...' : 'Create tag...'}
+            onFocus={() => setMode('search')}
           />
         </div>
       )}
-      {tagManagerState == 'searchTag' ? (
-        <div className={styles.expandedTagsMenu}>
-          <span className={styles.tagType}>Recent</span>
-          <ul className={styles.availableTagsList}>
-            <li className={styles.availableTag}>
-              <span
-                style={{
-                  color: '#800080',
-                  // backgroundColor: hexToRgba('#800080', 0.075),
-                  backgroundColor: '#f7eef6',
-                }}
+      <div></div>
+      {isSearchTagOpened && (
+        <>
+          {mode == 'search' && (
+            <div className={styles.expandedTagsMenu}>
+              <span className={styles.tagType}>Recent</span>
+              <ul className={styles.availableTagsList}>
+                <li className={styles.availableTag}>
+                  <span
+                    style={{
+                      color: '#800080',
+                      // backgroundColor: hexToRgba('#800080', 0.075),
+                      backgroundColor: '#f7eef6',
+                    }}
+                  >
+                    <HiHashtag />
+                  </span>
+                  Job
+                </li>
+                <li className={styles.availableTag}>
+                  <span
+                    style={{
+                      color: '#fa8008',
+                      backgroundColor: hexToRgba('#fa8008', 0.075),
+                    }}
+                  >
+                    <HiHashtag />
+                  </span>
+                  Sport
+                </li>
+                <li className={styles.availableTag}>
+                  <span
+                    style={{
+                      color: '#1f790f',
+                      backgroundColor: hexToRgba('#1f790f', 0.075),
+                    }}
+                  >
+                    <HiHashtag />
+                  </span>
+                  Programmin
+                </li>
+              </ul>
+              <button
+                className={styles.addNewTag}
+                onClick={() => setMode('create')}
               >
-                <HiHashtag />
-              </span>
-              Job
-            </li>
-            <li className={styles.availableTag}>
-              <span
-                style={{
-                  color: '#fa8008',
-                  backgroundColor: hexToRgba('#fa8008', 0.075),
-                }}
-              >
-                <HiHashtag />
-              </span>
-              Sport
-            </li>
-            <li className={styles.availableTag}>
-              <span
-                style={{
-                  color: '#1f790f',
-                  backgroundColor: hexToRgba('#1f790f', 0.075),
-                }}
-              >
-                <HiHashtag />
-              </span>
-              Programmin
-            </li>
-          </ul>
-          <button
-            className={styles.addNewTag}
-            onClick={() => setTagManagerState('createTag')}
-          >
-            <span>
-              <Plus />
-            </span>
-            Add new tag
-          </button>
-        </div>
-      ) : (
-        <div className={styles.creationTagWrapper}>
-          <ColorPicker
-            value={color}
-            onChange={setColor}
-          />
-          <div className={styles.creationTagActionBtns}>
-            <Button
-              onClick={() => setTagManagerState('searchTag')}
-              variant="outlined"
-            >
-              Cancel
-            </Button>
-            <Button>Create</Button>
-          </div>
-        </div>
+                <span>
+                  <Plus />
+                </span>
+                Add new tag
+              </button>
+            </div>
+          )}
+          {mode == 'create' && (
+            <div className={styles.creationTagWrapper}>
+              <ColorPicker
+                value={color}
+                onChange={setColor}
+              />
+              <div className={styles.creationTagActionBtns}>
+                <Button
+                  onClick={() => setMode('search')}
+                  variant="outlined"
+                >
+                  Cancel
+                </Button>
+                <Button>Create</Button>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
