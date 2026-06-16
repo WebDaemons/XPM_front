83eb9c7e6516658f82a8ac0cb5f5e1776f5bf5e8
Mykhailo Verkhovod <michaelverkhovod@gmail.com>
2026-06-16 17:32:55 +0300

    fixed vite error for unused import

--- src/features/notes/NotesContent.jsx
+++ src/features/notes/NotesContent.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { useNotes } from './hooks/useNotes';
 
 import { NotesGrid } from '@features/notes/components/NotesGrid/NotesGrid';
-import { NotesToolbar } from '@features/notes/components/NotesToolbar/NotesToolbar';
 import { NotesHeader } from '@features/notes/components/NotesHeader/NotesHeader';
 import { NoteEditor } from '@features/notes/components/NoteEditor/NoteEditor';
 import { NotesWorkspace } from '@features/notes/components/NotesWorkspace/NotesWorkspace';
